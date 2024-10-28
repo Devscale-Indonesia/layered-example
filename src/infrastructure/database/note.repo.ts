57e14f7fc2ab00d85a8bdb54675fcbb98510795d
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-10-28 22:06:28 +0700

    finished layered

--- src/infrastructure/database/note.repo.ts
+++ src/infrastructure/database/note.repo.ts
@@ -1,24 +1,20 @@
-import { INote, Note } from "../../interfaces/entities";
+import { INote } from "../../interfaces/entities";
+import { Note } from "@prisma/client";
+import { prisma } from "../../utils/prisma";
 import { injectable } from "inversify";
 import "reflect-metadata";
 
 @injectable()
 export class NoteRepo implements INote {
   async getAll(): Promise<Note[]> {
-    return [
-      {
-        id: "test123",
-        content: "test",
-        isDone: false,
-      },
-    ];
+    return await prisma.note.findMany();
   }
 
-  async getOneById(): Promise<Note> {
-    return {
-      id: "test123",
-      content: "test",
-      isDone: false,
-    };
+  async getOneById(id: number): Promise<Note | null> {
+    return await prisma.note.findFirst({
+      where: {
+        id,
+      },
+    });
   }
 }
